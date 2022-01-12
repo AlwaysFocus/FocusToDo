d6a4e9276ad65d6054c17ab149240214ede77b6e
eliwhalen <85242655+eliwhalen@users.noreply.github.com>
2022-01-11 21:05:02 -0700

    Finished building animated task label

--- components/animated-task-label.tsx
+++ components/animated-task-label.tsx
@@ -12,8 +12,8 @@ interface Props {
 }
 
 const AnimatedBox = Animated.createAnimatedComponent(Box)
-const animatedHStack = Animated.createAnimatedComponent(HStack)
-const animatedText = Animated.createAnimatedComponent(Text)
+const AnimatedHStack = Animated.createAnimatedComponent(HStack)
+const AnimatedText = Animated.createAnimatedComponent(Text)
 
 const AnimatedTaskLabel = memo((props: Props) => {
     const {strikethrough, textColor, inactiveTextColor, onPress, children} = props
@@ -54,7 +54,42 @@ const AnimatedTaskLabel = memo((props: Props) => {
         const easing = Easing.out(Easing.quad)
 
         if(strikethrough) {
-            
+            hstackOffset.value = withSequence(
+                withTiming(4, {duration: 200, easing}),
+                withTiming(0, {duration: 200, easing})
+            )
+            strikethroughWidth.value = withTiming(1, {duration: 400, easing})
+            textColorProgress.value = withDelay(
+                1000,
+                withTiming(1, {duration: 400, easing})
+            )
+        } else {
+            strikethroughWidth.value = withTiming(0, {duration: 400, easing})
+            textColorProgress.value = withTiming(0, {duration: 400, easing})
         }
     })
+
+    return (
+        <Pressable onPress={onPress}>
+            <AnimatedHStack alignItems="center" style={[hstackAnimatedStyles]}>
+                <AnimatedText
+                 fontSize={19}
+                 noOfLines={1}
+                 isTruncated
+                 px={1}
+                 style={[textColorAnimatedStyles]}
+                >
+                    {children}
+                </AnimatedText>
+                <AnimatedBox 
+                 position="absolute"
+                 h={1}
+                 borderBottomWidth={1} 
+                 style={[strikethroughAnimatedStyles]}
+                 />
+            </AnimatedHStack>
+        </Pressable>
+    )
 })
+
+export default AnimatedTaskLabel
